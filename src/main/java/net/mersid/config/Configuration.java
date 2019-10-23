07745ad820648ea45b8e807227dedb9c33784592
Mersid <mersid_email@yahoo.com>
2019-10-22 23:37:01 -0700

    Update Configuration.java

--- src/main/java/net/mersid/config/Configuration.java
+++ src/main/java/net/mersid/config/Configuration.java
@@ -8,7 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Configuration {
-	Path savePath;
+	private Path savePath;
 
 	// Property names for json key-value pairs
 	private static final String
